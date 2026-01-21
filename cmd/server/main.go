9e61d8cf051ba898446df8e0ab123d18a44f66a5
yadukrishnan <yadhukrishnangta@gmail.com>
2026-01-21 11:18:24 +0530

    shutdown funtion and data base closing funtion implemented successfully

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/yadukrishnan2004/ecommerce-backend/internal/adapter/handler"
 	"github.com/yadukrishnan2004/ecommerce-backend/internal/adapter/notifications"
@@ -14,21 +22,30 @@ import (
 
 func main() {
 
-	//initilizing the fiber router
-	app:=fiber.New()
-
+	
 	//loading the env , setting the Port for runnint the server, setting the DSN 
 	cfg:=config.Load()
-
+	
 	// connecting the data base pass an dsn (data source name in the form of string)
 	DB:=infrastructure.ConnectPostgres(cfg.DSN)
-
-	//Auto migrate repositery tables 
-
-	DB.AutoMigrate(
-		&domain.User{},   //user table
-	)
-
+	sqlDb,err:=DB.DB()
+	if err != nil {
+		log.Fatal("fale to underlaying db connection")
+	}
+	
+	//initilizing the fiber router
+	app:=fiber.New(fiber.Config{
+			DisableStartupMessage: true, 
+		})
+		
+		
+		//Auto migrate repositery tables 
+		
+		DB.AutoMigrate(
+			&domain.User{},   //user table
+		)
+	
+	
 	// setting up the handler layer
 
 
@@ -52,6 +69,31 @@ func main() {
 
 	router.SetUpRouther(app,UserHandler)
 
-	app.Listen(":"+cfg.App_Port)
+//runing the server in an separate goroutine
+	go func(){
+		fmt.Println("Server is running on :3000")
+		if err:=app.Listen(":"+cfg.App_Port);err != nil {
+			log.Panic(err)
+		}
+	}()
 
+	c:=make(chan os.Signal,1)
+
+	signal.Notify(c,os.Interrupt,syscall.SIGTERM)
+	<-c
+	fmt.Println("The Sever is Starting shutting down....")
+
+		ctx,cancel:=context.WithTimeout(context.Background(),5*time.Second)
+		defer cancel()
+		
+		if err:=app.ShutdownWithContext(ctx);err !=nil{
+			fmt.Printf("Server force to Shutdown:%v\n",err)
+		}
+
+		fmt.Println("Closing data base connection...")
+		if err:=sqlDb.Close();err != nil {
+			fmt.Printf("Faile to Close the db:%v\n",err)
+
+		}
+		fmt.Println("Sever shutdown successfully")
 }
